c55a7b9a026172ee2f1a5bf9b4cb19bdcd0cb000
jeff-mccommas <jeffmccommas@gmail.com>
2018-01-21 05:14:12 -0500

    added ckeditor

--- src/app/components/admin-add-page/admin-add-page.component.ts
+++ src/app/components/admin-add-page/admin-add-page.component.ts
@@ -1,4 +1,9 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { PageService } from './../../services/page.service';
+
+declare var CKEDITOR: any;
 
 @Component({
   selector: 'app-admin-add-page',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAddPageComponent implements OnInit {
 
-  constructor() { }
+  public successMsg: boolean = false;
+  public errorMsg: boolean = false;
+  public title: string;
+  public content: string;
+
+  constructor(
+    private router: Router,
+    private pageService: PageService
+  ) { }
 
   ngOnInit() {
+
+    if (localStorage.getItem("user") !== "\"jeffmccommas@gmail.com\"") {
+      this.router.navigateByUrl('');
+    } else {
+      CKEDITOR.replace('content');
+    }
+
+  }
+
+  addPage({form, value, valid}) {
+    form.reset();
+    if (valid) {
+      value.content = CKEDITOR.instances.content.getData();
+      this.pageService.postAddPage(value).subscribe(res => {
+        if (res == 'pageExists') {
+          this.errorMsg = true;
+          setTimeout(function() {
+            this.errorMsg = false;
+          }.bind(this),2000);
+        } else {
+          this.successMsg = true;
+          setTimeout(function() {
+            this.successMsg = false;
+          }.bind(this),2000);
+
+          CKEDITOR.instances.content.setData('');
+
+          this.pageService.getPages().subscribe(pages => {
+            this.pageService.pagesBS.next(pages);
+          })
+        }
+      });
+    } else {
+      console.log('Form is not valid.');
+    }
   }
 
 }
--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -1,4 +1,4 @@
-// import { Http } from '@angular/http';
+import { Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 // import 'rxjs/add/operator/map';
@@ -8,46 +8,46 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PageService {
 
-    // constructor(private http: Http) { }
-    constructor(private http: HttpClient) { }
+  // constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
-    // public pagesBS = new BehaviorSubject<string>(null);
-    public pagesBS = new BehaviorSubject<Object>(null);
+  // public pagesBS = new BehaviorSubject<string>(null);
+  public pagesBS = new BehaviorSubject<Object>(null);
 
-    getPages() {
-        // return this.http.get('http://localhost:3000/pages')
-        //     .map(res => res.json());
-        return this.http.get('http://localhost:3000/pages');
-    }
+  getPages() {
+    // return this.http.get('http://localhost:3000/pages')
+    //     .map(res => res.json());
+    return this.http.get('http://localhost:3000/pages');
+  }
 
-    getPage(slug) {
-        // return this.http.get('http://localhost:3000/pages/' + slug)
-        //     .map(res => res.json());
-        return this.http.get('http://localhost:3000/pages/' + slug);
-    }
+  getPage(slug) {
+    // return this.http.get('http://localhost:3000/pages/' + slug)
+    //     .map(res => res.json());
+    return this.http.get('http://localhost:3000/pages/' + slug);
+  }
 
-    postAddPage(value) {
-        // return this.http.post('http://localhost:3000/pages/add-page', value)
-        //     .map(res => res.json());
-        return this.http.post('http://localhost:3000/pages/add-page', value);
-    }
+  postAddPage(value) {
+    // return this.http.post('http://localhost:3000/pages/add-page', value)
+    //     .map(res => res.json());
+    return this.http.post('http://localhost:3000/pages/add-page', value);
+  }
 
-    getEditPage(id) {
-        // return this.http.get('http://localhost:3000/pages/edit-page/' + id)
-        //     .map(res => res.json());
-        return this.http.get('http://localhost:3000/pages/edit-page/' + id);
-    }
+  getEditPage(id) {
+    // return this.http.get('http://localhost:3000/pages/edit-page/' + id)
+    //     .map(res => res.json());
+    return this.http.get('http://localhost:3000/pages/edit-page/' + id);
+  }
 
-    postEditPage(value) {
-        // return this.http.post('http://localhost:3000/pages/edit-page/'+value.id, value)
-        //     .map(res => res.json());
-        return this.http.post('http://localhost:3000/pages/edit-page/'+value.id, value);
-    }
+  postEditPage(value) {
+    // return this.http.post('http://localhost:3000/pages/edit-page/'+value.id, value)
+    //     .map(res => res.json());
+    return this.http.post('http://localhost:3000/pages/edit-page/'+value.id, value);
+  }
 
-    deletePage(id) {
-        // return this.http.get('http://localhost:3000/pages/delete-page/' + id)
-        //     .map(res => res.json());
-        return this.http.get('http://localhost:3000/pages/delete-page/' + id);
-    }
+  deletePage(id) {
+    // return this.http.get('http://localhost:3000/pages/delete-page/' + id)
+    //     .map(res => res.json());
+    return this.http.get('http://localhost:3000/pages/delete-page/' + id);
+  }
 
 }
